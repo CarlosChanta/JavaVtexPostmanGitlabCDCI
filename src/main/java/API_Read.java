71e3d4842c97f507301efe635c78f709506fc8de
Carlos Chanta <carloschanta152@gmail.com>
2019-08-24 02:04:55 -0500

    Integracion terminada de repo a vtex, falta capturar cookie

--- src/main/java/API_Read.java
+++ src/main/java/API_Read.java
@@ -1,3 +1,5 @@
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -8,18 +10,18 @@ public class API_Read {
 
     public static void main(String[] args)
     {
-        String file= "main-promart.css";
-        String repopath= "dev%2Ffiles%2F";
-        String branch = "master";
-        //getContentFileRepo(file,repopath,branch);
-
-
-        //getListRepoPaths("source/js");
-
-        String ListaVTEX = ListarArchivosVTEX();
-
+        String galletaVTEX = "VtexRCMacIdv7=0d4abe40-a86b-11e9-8461-4be2b44df238; _ga=GA1.2.414783797.1564880657; _hjid=44928b47-fc91-426c-8270-e0511ba860e4; intercom-id-bs8us8hw=6a8abb2a-1daf-4b79-a435-7457a394f4e1; vtex_topbar_pic=0-0; _ga=GA1.3.414783797.1564880657; VtexFingerPrint=fd2a98dc4477751375799d4b43e9d0e4; janus_sid=5355bcb2-e59f-414f-b2cf-c24127ac240e; vtex_segment=eyJjYW1wYWlnbnMiOm51bGwsImNoYW5uZWwiOiIyIiwicHJpY2VUYWJsZXMiOm51bGwsInJlZ2lvbklkIjpudWxsLCJ1dG1fY2FtcGFpZ24iOm51bGwsInV0bV9zb3VyY2UiOm51bGwsInV0bWlfY2FtcGFpZ24iOm51bGwsImN1cnJlbmN5Q29kZSI6IlBFTiIsImN1cnJlbmN5U3ltYm9sIjoiUy8iLCJjb3VudHJ5Q29kZSI6IlBFUiIsImN1bHR1cmVJbmZvIjoiZXMtUEUiLCJhZG1pbl9jdWx0dXJlSW5mbyI6ImVuLVVTIn0; _gid=GA1.3.1006403163.1566529130; _gid=GA1.2.1006403163.1566529130; _vss=9512E7744DFAD6CEB6B4DB78E9AC8DE9E9059D3C36579AF6D625ADE78E239C78; VtexRCSessionIdv7=0%3A6287bee0-c61f-11e9-ac6a-59430fc4249a; i18next=es-ES; VtexIdclientAutCookie=eyJhbGciOiJFUzI1NiIsImtpZCI6IjcyMTE1REY3RTVFNDJEMjZGMDgzNTBBQjRCODI0NTNENEE2QURDNUMiLCJ0eXAiOiJqd3QifQ.eyJzdWIiOiJjYXJsb3MuY2hhbnRhQHByb21hcnQucGUiLCJhY2NvdW50IjoiX192dGV4X2FkbWluIiwic2NvcGUiOiJwcm9tYXJ0OmFkbWluIiwiYXV0aG9yaXphYmxlcyI6WyJ2cm46aWFtOl9fdnRleF9hZG1pbjp1c2Vycy9jYXJsb3MuY2hhbnRhQHByb21hcnQucGUiXSwiZXhwIjoxNTY2NzAzNzg3LCJ1c2VySWQiOiJjNjdkYjA3Yi0wZDgxLTQzYmMtOThlNy04ZTI1YzRkNmQ4MjMiLCJhdXRoX2x2bCI6InN0cm9uZyIsImlhdCI6MTU2NjYxNzM4NywiaXNzIjoidG9rZW4tZW1pdHRlciIsImp0aSI6IjRhODNmYjU2LTJmMTAtNGUxYy04MmEyLTNiNWEyZDkxZGM0NSJ9.zZgDO362131G1xbhz6vDMufN6bkkkIJ0Y5eDkNBAubvQXOOu2AVU5dR9VuicjDFeqqVTYcb7YT-7AhRI6J0qWQ; vtex_session=eyJhbGciOiJFUzI1NiIsImtpZCI6IkJGMjM5Q0E0REM1RURDQUFCNUQ4MTJEOThFODMzQjk0MURCM0JEQTgiLCJ0eXAiOiJqd3QifQ.eyJhY2NvdW50LmlkIjoiYmM2YTcxYTgtOTQ3Yy00ZmViLWEwMDctZTQzNmRkYjNjZjZlIiwiaWQiOiI1OTAwMDgyMy0xMzhmLTRiZGYtODJkNy1iNWY4NzY4MTYyODEiLCJ2ZXJzaW9uIjozLCJzdWIiOiJzZXNzaW9uIiwiYWNjb3VudCI6InNlc3Npb24iLCJleHAiOjE1NjczMDg2MjIsImlhdCI6MTU2NjYxNzQyMiwiaXNzIjoidG9rZW4tZW1pdHRlciIsImp0aSI6ImViNDkzODE5LWJiNjgtNDE5YS04ZGRjLWYzMTQ0NTg0NmY0MCJ9.fFthwTOyfRaEhwvk49HtHuxk_h4YSrYNVuj_XQsyuMaqgzO1sbotjZStScxrQgmdrdhNmT9LeaX9Ps8p8DCDYg; _hjIncludedInSample=1; VtexRCRequestCounter=3; intercom-session-bs8us8hw=VEp4NGFtR25GRmZNa0RTUTFnSXV6RFpYZllQUHYvd1VFRUc0UThxc25yRC9UMlRPYjh1UW0vK2p5WG1zbnQyaS0tdGdxbUVuVDJueGRnWEJIa2toVm1lUT09--239e067312368959701bc21c358d30c9585348ae; _gat_UA-43760863-20=1";
+        String ListaVTEX = ListarArchivosVTEX(galletaVTEX);
         String ListaRepo = ListarArchivosRepo();
+        IntegracionREPOaVTEX(ListaVTEX,ListaRepo,galletaVTEX);
 
+    }
+
+    public static void IntegracionREPOaVTEX(String ListaVTEX,String ListaRepo,String galletaVTEX)
+    {
+        //FUNCION QUE SUBE ARCHIVOS DE REPO A VTEX
+        //SI EL ARCHIVO ESTA EN EL REPO Y EN VTEX, SE OBTIENE SU CONTENIDO Y SE ACTUALIZA
+        //SI EL ARCHIVO ESTA EN EL REPO, PERO NO EN VTEX, SE CREA EL ARCHIVO EN EL OMS DE VTEX EN CHECKOUT/CODIGO
 
         JSONArray VTEXFiles = new JSONArray(ListaVTEX);
         JSONArray RepoFiles = new JSONArray(ListaRepo);
@@ -27,31 +29,47 @@ public class API_Read {
         for (int i = 0; i <RepoFiles.length() ; i++) {
             JSONObject name = RepoFiles.getJSONObject(i);
             String fileRepo = name.getString("name");
-            System.out.println(fileRepo);                       //SOLO PARA VERIFICAR
-            for (int j = 0; j < VTEXFiles.length(); j++) {
-                
+            String filePath = name.getString("path");
+            for (int k = 0; k < VTEXFiles.length(); k++) {
+                String fileVTEX = VTEXFiles.getString(k);
+                if (fileRepo.equals(fileVTEX)){
+                    System.out.println(fileRepo+"="+fileVTEX+"    ARCHIVO DE REPO ENCONTRADO!!!!");
+                    System.out.println("SUBIENDO A VTEX...");
+                    HttpResponse<JsonNode> response = Unirest.put("https://promart.myvtex.com/api/portal/pvt/sites/promart/files/" + fileRepo)
+                            .header("Content-Type", "application/json")
+                            .header("Accept", "application/json, text/plain")
+                            .header("Cookie", galletaVTEX)
+                            .body(getContentFileRepo(fileRepo,filePath,"master"))
+                            .asJson();
+                    if (response.isSuccess()){
+                        System.out.println("ARCHIVO SUBIDO CON EXITO");
+                        break;
+                    }else{
+                        System.out.println("FALLO LA SUBIDA DE ARCHIVO, REVISE RESPUESTA DE API");
+                        break;
+                    }
+                }
             }
         }
-
-
-
     }
 
     public static String ListarArchivosRepo()
     {
+        //ESTA FUNCION CONCATENA TODOS LOS NOMBRES DE ARCHIVO Y RUTA OBTENIDAS DEL METODO getListRepoPaths
+        //EL ARCHIVO STRING RepoFiles esta en formato JSON PARA SU USO POSTERIOR
         String RepoFiles="[";
-
         RepoFiles=RepoFiles+getListRepoPaths("dev/files")+",";
         RepoFiles=RepoFiles+getListRepoPaths("source/js")+",";
         RepoFiles=RepoFiles+getListRepoPaths("source/json");
         RepoFiles=RepoFiles+"]";
-
         return RepoFiles;
     }
 
-    public static String ListarArchivosVTEX()
+    public static String ListarArchivosVTEX(String galletaVTEX)
     {
-        String galletaVTEX = "VtexRCMacIdv7=821ff380-8d21-11e9-a249-2f7b56cc9687; VtexFingerPrint=11a712643d97e54330868fec964e5bfb; _ga=GA1.2.1223822807.1560368651; vtex_topbar_pic=0-0; _ga=GA1.3.1223822807.1560368651; checkout.vtex.com=__ofid=59682cceb3b64b9ba1db0742dd0b58ec; .ASPXAUTH=5A333F14B2AEB3C39D27258FE88B419ADF9036B15D1A2CB1AFD0562458E92088EF83BDAC202969EA24392C70E3EB4BB2B01B336341B68FAAC0316ADC01E23503FF1F646B78255522630FC247FF730EDCEA06514E62E311FE788C55B75E6445293CF34C8D27F9468EDB5CABE8EA2029DB5932DCB70A57B10FFAD52F149BB5950B3F40F11095D4359B85BD94896A94D921097B758E6141619E624FA54E8CDFB3443F9F6115; databot={\"ENCRYPTED_DATA\":\"\",\"utm_source\":\"\",\"email\":\"\"}; intercom-id-bs8us8hw=8a87d987-38a4-439a-9899-6dacafebfa14; IPI=UsuarioGUID=c67db07b-0d81-43bc-98e7-8e25c4d6d823; _hjid=725b00d6-a65b-40d9-b544-fd45ec907f16; vtex_segment=eyJjYW1wYWlnbnMiOm51bGwsImNoYW5uZWwiOiIyIiwicHJpY2VUYWJsZXMiOm51bGwsInJlZ2lvbklkIjpudWxsLCJ1dG1fY2FtcGFpZ24iOm51bGwsInV0bV9zb3VyY2UiOm51bGwsInV0bWlfY2FtcGFpZ24iOm51bGwsImN1cnJlbmN5Q29kZSI6IlBFTiIsImN1cnJlbmN5U3ltYm9sIjoiUy8iLCJjb3VudHJ5Q29kZSI6IlBFUiIsImN1bHR1cmVJbmZvIjoiZXMtUEUiLCJhZG1pbl9jdWx0dXJlSW5mbyI6ImVuLVVTIn0; i18next=es-ES; _gid=GA1.2.836520032.1566230862; janus_sid=9a26c3b7-be3f-443f-ab96-89295906c1bf; VtexIdclientAutCookie=eyJhbGciOiJFUzI1NiIsImtpZCI6IjI3RkM5MUQwRENCMjJFRDI5RTVCQjU3OUZGRThFQUE0ODRGRTkxOUIiLCJ0eXAiOiJqd3QifQ.eyJzdWIiOiJjYXJsb3MuY2hhbnRhQHByb21hcnQucGUiLCJhY2NvdW50IjoiX192dGV4X2FkbWluIiwic2NvcGUiOiJwcm9tYXJ0OmFkbWluIiwiYXV0aG9yaXphYmxlcyI6WyJ2cm46aWFtOl9fdnRleF9hZG1pbjp1c2Vycy9jYXJsb3MuY2hhbnRhQHByb21hcnQucGUiXSwiZXhwIjoxNTY2NjAwMjUzLCJ1c2VySWQiOiJjNjdkYjA3Yi0wZDgxLTQzYmMtOThlNy04ZTI1YzRkNmQ4MjMiLCJhdXRoX2x2bCI6InN0cm9uZyIsImlhdCI6MTU2NjUxMzg1MywiaXNzIjoidG9rZW4tZW1pdHRlciIsImp0aSI6ImQzYTM4MzJhLTE5ODgtNDJiZC1iNTVlLTk1ZmE4MmE5OTk1YSJ9.szAHdZLA_rcYUpQZBE64yEEG2GtHk4MS71bXkT9r4CpkxcqrJ79R4woI09QS1pMyP8bk51gj9G1mLJ5zhHE7JA; vtex_session=eyJhbGciOiJFUzI1NiIsImtpZCI6IjFGOTdDMjQ4QzI0NjQzMzc1QTcyREE3MTEwQTA1REIwM0I3OUJGQjEiLCJ0eXAiOiJqd3QifQ.eyJhY2NvdW50LmlkIjoiYmM2YTcxYTgtOTQ3Yy00ZmViLWEwMDctZTQzNmRkYjNjZjZlIiwiaWQiOiI5OGYyZWJlZi03MjNmLTQyMjMtOThlNC05NTc0Y2VmMDU2ZjQiLCJ2ZXJzaW9uIjoxNiwic3ViIjoic2Vzc2lvbiIsImFjY291bnQiOiJzZXNzaW9uIiwiZXhwIjoxNTY3MjA1MDY0LCJpYXQiOjE1NjY1MTM4NjQsImlzcyI6InRva2VuLWVtaXR0ZXIiLCJqdGkiOiJhMmJmYzQ5Mi03ZGQ0LTRjZTYtOWU4My0yNTM2ZDQyOTliZWUifQ.qpB-zHOqrEc8RpkYUYiKIcvJ6eTTzvMFiN2-3yVIKK8pSdpV-xJdpWjPvvuLpziG6PMBnMvQhRm7Vb4GPlNjZg; _gid=GA1.3.836520032.1566230862; intercom-session-bs8us8hw=VisxNDlub09IWURJaHJnMjMyaEYxY3dleURZTlYxSkhOa0dwdnlZQVplWDZmbXk5K3B2c1BhWHdKZm1RZGFrKy0tU1YzdFZ1R01wYUhiblBNbHFtMm40Zz09--bf3842d7ca5d67065c468ccd7f317f4e83355cf1; _gat_UA-43760863-20=1";
+        //ESTA FUNCION DEVUELVE UNA LISTA DE TODOS LOS ARCHIVOS EN VTEX EN LA SECCION CHECKOUT/CODIGO DEL OMS DEL PROPIO VTEX
+        //EJECUTANDO UN API DE VTEX
+        //HAY QUE ENVIARLE LA COOKIE DE LA SESION DE ADMIN INICIADA
 
         String VTEXFiles = Unirest.get("https://promart.myvtex.com/api/portal/pvt/sites/promart/files")
                 .header("Cookie",galletaVTEX)
@@ -62,6 +80,10 @@ public class API_Read {
 
     public static String getListRepoPaths(String path)
     {
+        //ESTA FUNCION OBTIENE LA LISTA DE ARCHIVOS DE UNA RUTA QUE LE PASEMOS (path)
+        //Y LA DEVUELVE EN UN STRING (arrayInit) QUE CONTIENE RUTA Y NOMBRE DE ARCHIVO
+        //arrayInit SE DEVUELVE COMO STRING, PERO ESTA EN FORMATO JSON PARA SU USO POSTERIOR
+
         String GetListRepoPaths = Unirest.get("https://gitlab.com/api/v4/projects/12239356/repository/tree")
                 .header("PRIVATE-TOKEN","GVVGLD4z4w1vRJ24UYov")
                 .queryString("path",path)
@@ -86,8 +108,15 @@ public class API_Read {
         return arrayInit;
     }
 
-    public static void getContentFileRepo(String file,String repopath,String branch)
+    public static String getContentFileRepo(String file,String repopath,String branch)
     {
+        //ESTA FUNCION EJECUTA EL API DE GITLAB
+        //QUE OBTIENE DATOS DE UN ARCHIVO EN UN REPOSITORIO INCLUYENDO SU CONTENIDO
+        //SE PASAN COMO PARAMETROS EL NOMBRE DE ARCHIVO (file), SU DIRECCION EN EL REPO (path) Y LA RAMA DE LA CUAL DE LO SACAMOS (branch)
+        //EL CONTENIDO DEL ARCHIVO LLEGA CODIFICADO EN BASE64 POR LO CUAL SE DECODIFICA
+        //Y LA FUNCION DEVUELVE UN STRING QUE EN REALIDAD ESTA EN FORMATO JSON, PARA QUE SE ENVIE
+        //EN LA PETICION DEL API QUE HACE LA SUBIDA A VTEX
+
         String GetContentFileRepo = Unirest.get("https://gitlab.com/api/v4/projects/12239356/repository/files/"+repopath+file)
                 .header("PRIVATE-TOKEN","GVVGLD4z4w1vRJ24UYov")
                 .queryString("ref",branch)
@@ -96,10 +125,8 @@ public class API_Read {
 
         JSONObject Jfile = new JSONObject(GetContentFileRepo);
         String text = Jfile.getString("content");
-
         byte[] valueDecoded = Base64.decodeBase64(text);
         String jsonLoadVtex = "{\"path\":\""+file+"\",\"text\":\""+new String(valueDecoded)+"\"}";
-
-        System.out.println(jsonLoadVtex);
+        return  jsonLoadVtex; //SE RETORNA EL JSON QUE SE USARA PARA EL API QUE HACE LA SUBIDA A VTEX
     }
 }
